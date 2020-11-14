5176186f3c86fbfb5827714467fa41e84c6842c6
RocioRojas <rociorojas391@gmail.com>
2020-11-13 23:46:47 -0400

    Archivo modificado 13/11/20 modifique la planta y con el pid de matlab busque el valor de kp ki kd, no esta funcionando la prueba con el arduino

---
+++ ARDUINO/Pid301020/Pid301020.ino
@@ -0,0 +1,96 @@
+
+
+
+float cError,cErrorAnt=0.0,Kp=0.14475,Ki=2.87,Kd=0.00034395,a,UdAnt=0.0,UiAnt=0.0,Up,Ui,Ud,Ut,Tm=0.002/100,N=0;
+int Ref=100;
+float z,zant=0,zaux,u,g1,b1,y1,F;
+float R=1.5;
+int aux,Read, i=0;
+float fAuxLectura;
+char buff[100];
+
+char Arredatos[100];
+
+unsigned long entero;
+bool flag = false;
+void setup() {
+
+
+
+
+a=Kd/(Kd+N*Tm);
+
+
+Serial.begin(57600);
+
+}
+
+void loop() {
+
+//if (Read==0x04){
+//flag = true;
+//}
+//else if (Read==0x08)
+//{
+//  flag=false;
+// }  
+
+// if(flag)
+if(Serial.available())
+{
+Read=Serial.read();
+
+
+Arredatos[i]=Read;
+
+
+
+if (Arredatos[i]=='\n'){
+entero=atol(Arredatos);
+fAuxLectura=entero/100.0;
+//fAuxLectura*=255.0/5.0;   
+cError=Ref-fAuxLectura;
+//    Serial.println(cError);
+Up=Kp*cError;
+ 
+Ud=Kd*(cError-cErrorAnt)/Tm;
+Ui=UiAnt+Ki*cError*Tm;
+Ut= Up+Ud+Ui;
+//    Serial.println(Up);
+//    Serial.println(Ud);
+//    Serial.println(Ui);
+//    Serial.println(Ut);
+if (Ut>255.0) Ut=255.0;
+if (Ut<0.0) Ut=0.0;
+cErrorAnt=cError;
+UdAnt=Ud;
+UiAnt=Ui;
+
+// Ut=999.9916;
+entero=Ut*100;
+//Serial.print(entero);
+sprintf(buff,"%d\n",entero) ;
+//Serial.println("Datos: ");
+//Serial.print(Arredatos);
+
+//       Serial.println(convertirFloat);
+//entero=fAuxLectura*100;
+
+Serial.print(buff);
+
+
+memset(Arredatos, '\0', sizeof(Arredatos));
+i=0;
+}
+else{
+i++;
+}
+
+//Serial.print(Read) ;
+//Serial.write(57);
+
+}
+
+
+//delay(3000);       
+}
