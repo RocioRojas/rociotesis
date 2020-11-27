ce55fc47098c0f5e5426025fe48797e01ccde77e
RocioRojas <rociorojas391@gmail.com>
2020-11-27 12:33:28 -0400

    Agregando el archivo python

---
+++ python/Pruebacompleta191020.py
@@ -0,0 +1,47 @@
+import serial
+import matplotlib.pyplot as plt
+from scipy.signal import lsim
+import numpy as np
+from scipy.signal import lti
+import sys
+ser = serial.Serial()  # open serial port
+ser.baudrate=57600
+ser.timeout=10
+ser.port="COM9"
+ser.open()
+
+
+A = np.array([[0, 1E4*0.2340], [-0.0061*1E4, -1E4*1.2927]])
+B = np.array([[0.0], [1.2195*1E3]])
+C = np.array([[1.0, 0.0]])
+D = 0.0
+system = lti(A, B, C, D)
+u=np.array([0])
+
+while True:
+	lect=ser.readline()
+	print(lect)
+	if lect=='':
+		u=np.append(u,0)
+	else:
+	
+		print("antes")
+		print(u)
+		u=np.append(u,int(lect))
+		print("despues")
+		print(u)
+	t = np.linspace(0, 0.1, num=len(u))
+	if lect!="":
+		tout, y, x = lsim(system, u, t)
+		print(y)
+		b=int(y[len(y)-1])
+		print(b)
+		print("enviando...")
+		# aux=' '.join(map(str, b))
+		env=ser.write(b)
+		print(env)
+
+#			plt.plot(t, y)
+#			plt.grid(alpha=0.3)
+#			plt.xlabel('t')
+#			plt.show()
