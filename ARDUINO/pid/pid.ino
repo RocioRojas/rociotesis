26fb4e8858f57e2573952c3bde8e016c3e4c96ca
RocioRojas <rociorojas391@gmail.com>
2021-03-09 20:51:38 -0400

    Pid unificado

--- ARDUINO/pid/pid.ino
+++ ARDUINO/pid/pid.ino
@@ -10,7 +10,7 @@
 #define pin5 7
 #define pin6 8
 #define pin7 9
-
+#define bitEntrada A0
 float fError, fError_1 = 0, fError_2 = 0;
 float kp = 0.8817, ki = 0.7234, kd = 2.689 / 1000000;
 float ut, ut_1;
@@ -18,9 +18,9 @@ float Tm = 0.61 / 1000;
 float ref = 1, fLecturaAux;
 bool flag;
 int iLectura, i = 0;
-char buff[100], datos[100];
+char buff[100], datos[5];
+char *ptr;
 unsigned long entero;
-
 void writeBin(long valor);
 
 void setup()
@@ -33,31 +33,78 @@ void setup()
   pinMode(pin5, OUTPUT);
   pinMode(pin6, OUTPUT);
   pinMode(pin7, OUTPUT);
-
-  // Serial.begin(115200);
+  pinMode(bitEntrada, INPUT);
+  Serial.begin(115200);
 }
 
 void loop()
 {
+  if (digitalRead(bitEntrada) == 1)
+  {
+    if (Serial.available())
+    {
+      iLectura = Serial.read();
+      if (iLectura == CMD_ABRIR_COMUNICACION)
+      {
+        flag = true;
+        ut = 0;
+        sprintf(buff, "%d\n", ut);
+        Serial.print(buff);
+      }
+      else if (iLectura == CMD_CERRAR_COMUNICACION)
+      {
+        flag = false;
+      }
+      if (flag == true && iLectura != CMD_ABRIR_COMUNICACION)
+      {
+        datos[i] = iLectura;
+        if (datos[i] == '\n')
+        {
+          entero = strtol(datos, &ptr, 10);
+          fLecturaAux = entero / 100.0;
+          fError = ref - fLecturaAux;
+          ut = ut_1 + (kp + ki + kd) * fError - (2 * kd + kp) * fError_1 + kd * fError_2;
 
-  fLecturaAux = analogRead(A4) * 5 / 1023;
-  fError = ref - fLecturaAux;
-  ut = (ut_1 + (kp + ki + kd) * fError - (2 * kd + kp) * fError_1 + kd * fError_2);
-  //  ut=kp*fError;
-  if (ut > 5.0)
-    ut = 5.0;
+          if (ut > 5.0)
+            ut = 5.0;
+          /*
+        if (ut < 0.0)
+          ut = 0.0;
+          */
+          ut_1 = ut;
+          fError_2 = fError_1;
+          fError_1 = fError;
+          entero = ut * 100;
 
-  if (ut < 0.0)
-    ut = 0.0;
+          sprintf(buff, "%d\n", entero);
+          Serial.print(buff);
+          memset(datos, '\0', sizeof(datos));
+          i = 0;
+        }
+        else
+        {
+          i++;
+        }
+      }
+    }
+  }
+  else
+  {
+    fLecturaAux = analogRead(A4) * 5 / 1023;
+    fError = ref - fLecturaAux;
+    ut = (ut_1 + (kp + ki + kd) * fError - (2 * kd + kp) * fError_1 + kd * fError_2);
 
-  ut_1 = ut;
-  fError_2 = fError_1;
-  fError_1 = fError;
-  // analogWrite(3, 0.5 * 255 / 5);
-  writeBin(ut * 255 * 0.2);
-  delayMicroseconds(61);
+    if (ut > 5.0)
+      ut = 5.0;
+
+    ut_1 = ut;
+    fError_2 = fError_1;
+    fError_1 = fError;
+
+    writeBin(ut * 255 * 0.2);
+    delayMicroseconds(610);
+  }
 }
-
 void writeBin(long valor)
 {
 
